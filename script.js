60cec94cd0b381b8bf60d02d8ccf3f38bde137e0
POWER-OF-EVENT-ORGANIZING <1@ksapoe.com>
2025-07-05 00:43:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+function whatsapp() {
+    window.open("http://wa.me/966548504418")
+}
+
+function Email() {
+    window.open("mailto:1@ksapoe.com")
+}
+
